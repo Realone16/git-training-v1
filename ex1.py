79777749fa2c08b8d0b76787ca69b55af7fa551b
Realone <bakarerildwan4@gmail.com>
2022-04-06 13:05:11 +0100

    2nd commit:modified ex1 and ex2 python files

---
+++ ex1.py
@@ -0,0 +1,11 @@
+n = int(input('Enter the value of n:'))
+def count_to_n (x=n):
+    for i in range(1, x+1):
+        print (i, end='')
+    print()
+
+print('Going to count to () ...',format(n))
+count_to_n(n)
+#........................................
+print('First modification for 2nd commit')
+#.........................................
